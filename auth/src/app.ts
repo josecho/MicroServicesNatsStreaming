34b7c2f3a9396c3b8f0302e2cf3fb4ef7d371c0a
josecho <jlvbalsa@gmail.com>
2022-11-29 12:54:12 +0100

    update auth module

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-
 import cookieSession from 'cookie-session';
-
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler, NotFoundError } from '@josechotickets/common';
+import { errorHandler, NotFoundError } from '@jlvbcooptickets/common';
 
 const app = express();
 app.set('trust proxy', true);
